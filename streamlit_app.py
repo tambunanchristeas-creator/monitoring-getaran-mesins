e35b666629389bdad79d6535e7faadbb7243a870
tambunanchristeas-creator <tambunanchristeas@gmail.com>
2026-02-10 14:50:36 +0000

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,32 @@
 import streamlit as st
+import requests
 
-st.set_page_config(page_title="Monitoring Mesin", layout="wide")
+SUPABASE_URL = "https://qpefflvoxwtbqssimbev.supabase.co"
+SUPABASE_KEY = "sb_publishable_sqyi_4r3w3JiIR8wTyLG9g_0_oMexT7"
+TABLE = "maintable"
+
+HEADERS = {
+    "apikey": SUPABASE_KEY,
+    "Authorization": f"Bearer {SUPABASE_KEY}"
+}
 
 st.title("⚙️ Monitoring Getaran Mesin")
-st.write("Website berhasil jalan 🚀")
+
+url = f"{SUPABASE_URL}/rest/v1/{TABLE}?select=*&order=created_at.desc&limit=1"
+res = requests.get(url, headers=HEADERS)
+
+if res.status_code != 200:
+    st.error("Gagal ambil data")
+    st.stop()
+
+data = res.json()
+
+if len(data) == 0:
+    st.warning("Belum ada data")
+    st.stop()
+
+d = data[0]
+
+st.metric("RPM", d["rpm"])
+st.metric("Getaran", d["vibration"])
+st.metric("Status", d["status"])
