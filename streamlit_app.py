4436884e49c3276801122c17885b51d3cd07ae4a
tambunanchristeas-creator <tambunanchristeas@gmail.com>
2026-03-27 17:47:12 +0000

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -70,7 +70,7 @@ df = load_data()
 # ======================
 # FORMAT DATA BIAR RAPI
 # ======================
-df["TIME"] = pd.to_datetime(df["TIME"]).dt.strftime("%d-%m-%Y %H:%M:%S")
+df["TIME"] = pd.to_datetime(df["TIME"]).dt.strftime("%H:%M\n%b %d, %Y")
 
 # Pembulatan angka
 df["RPM"] = df["RPM"].astype(int)
