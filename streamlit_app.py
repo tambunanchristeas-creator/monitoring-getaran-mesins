4468867621b4d10d5a24bfaa57f50d31a7034898
tambunanchristeas-creator <tambunanchristeas@gmail.com>
2026-03-27 18:02:56 +0000

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,9 +5,9 @@ import plotly.express as px
 from streamlit_autorefresh import st_autorefresh
 
 # ======================
-# AUTO REFRESH 0,2 DETIK
+# AUTO REFRESH 0,5 DETIK
 # ======================
-st_autorefresh(interval=200, key="refresh")
+st_autorefresh(interval=500, key="refresh")
 
 # ======================
 # KONFIGURASI HALAMAN
