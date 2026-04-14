e9dd5d56bb95b444f81fbe030c8ec192a9f001d9
tambunanchristeas-creator <tambunanchristeas@gmail.com>
2026-04-14 07:59:03 +0000

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -145,28 +145,15 @@ with col2:
         with col_btn1:
             if st.button("🛑 MATIKAN MESIN", use_container_width=True):
                 try:
-                    supabase.table("monitoring").insert({
-                        "RPM": rpm,
-                        "Vrms": vibration,
-                        "STATUS": status,
-                        "D310": 1
-                    }).execute()
-
+                    supabase.table("control").insert({"D310": int(1)}).execute()
                     st.success("Perintah STOP dikirim!")
                 except Exception as e:
                     st.error(f"Gagal: {e}")
-
         with col_btn2:
             if st.button("▶️ HIDUPKAN MESIN", use_container_width=True):
                 try:
-                    supabase.table("monitoring").insert({
-                        "RPM": rpm,
-                        "Vrms": vibration,
-                        "STATUS": status,
-                        "D310": 0
-                    }).execute()
-
-                    st.success("Perintah RUN dikirim!")
+                    supabase.table("control").insert({"D310": int(0)}).execute()
+                    st.success("Perintah RUN Dikirim!")
                 except Exception as e:
                     st.error(f"Gagal: {e}")
 st.divider()
